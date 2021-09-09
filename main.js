2459c2c5577258a3dc3d16919c51be3e0ca3bfc3
unknown <bogyuni@Dong-A.com>
2021-09-09 16:03:26 +0900

    App - 파일생성과 리다이렉션

--- main.js
+++ main.js
@@ -82,13 +82,15 @@ var app = http.createServer(function(request, response){
 			body = body + data;
 		});
 
-		response.on('end', function () {
+		request.on('end', function () {
 			var post = qs.parse(body);
 			var title = post.title;
 			var description = post.description;
+			fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+				response.writeHead(302, {Location: `/?id=${title}`});
+				response.end();
+			});
 		});
-		response.writeHead(200);
-		response.end('seccess');
 } else {
 		response.writeHead(404);
 		response.end('Not found');
