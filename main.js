f24fa2d0fde33f345d1044a5799a9c5b0c97a562
bogyuni <bogyuni@hotmail.com>
2021-09-18 15:52:37 +0900

    App - 글삭제 - 삭제버튼 구현

--- main.js
+++ main.js
@@ -28,36 +28,40 @@ function templateList(filelist) {
 		list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
 		i = i + 1;
 	}
-	list = list+'</ul>';
+	list = list + '</ul>';
 	return list;
 }
 
-var app = http.createServer(function(request, response){
+var app = http.createServer(function (request, response) {
 	var _url = request.url;
 	var queryData = url.parse(_url, true).query;
 	var pathname = url.parse(_url, true).pathname;
 
-	if (pathname === '/'){
-		if (queryData.id === undefined){
-			fs.readdir('./data', function(error, filelist) {
+	if (pathname === '/') {
+		if (queryData.id === undefined) {
+			fs.readdir('./data', function (error, filelist) {
 				var title = 'Welcome!';
 				var description = 'Hello Node.js';
 				var list = templateList(filelist);
-				var template = templateHTML(title, list,
-					`<h2>${title}</h2>${description}`,
-					`<a href="/create">Create</a>`
-				);
+				var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">Create</a>`);
 				response.writeHead(200);
 				response.end(template);
 			});
 		} else {
-			fs.readdir('./data', function(error, filelist) {
+			fs.readdir('./data', function (error, filelist) {
 				fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
 					var title = queryData.id;
 					var list = templateList(filelist);
-					var template = templateHTML(title, list,
+					var template = templateHTML(
+						title,
+						list,
 						`<h2>${title}</h2>${description}`,
-						`<a href="/create">Create</a> <a href="/update?id=${title}">Update</a>`
+						`<a href="/create">Create</a>
+						<a href="/update?id=${title}">Update</a>
+						<form action="delete_process" method="post">
+							<input type="hidden" name="id" value="${title}">
+							<input type="submit" value="delete">
+						</form>`
 					);
 					response.writeHead(200);
 					response.end(template);
@@ -65,10 +69,12 @@ var app = http.createServer(function(request, response){
 			});
 		}
 	} else if (pathname === '/create') {
-		fs.readdir('./data', function(error, filelist) {
+		fs.readdir('./data', function (error, filelist) {
 			var title = 'WEB - Create';
 			var list = templateList(filelist);
-			var template = templateHTML(title, list,
+			var template = templateHTML(
+				title,
+				list,
 				`<form action="/create_process" method="post">
 				<p><input type="text" name="title" placeholder="Title"></p>
 				<p>
@@ -93,16 +99,18 @@ var app = http.createServer(function(request, response){
 			var title = post.title;
 			var description = post.description;
 			fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-				response.writeHead(302, {Location: `/?id=${title}`});
+				response.writeHead(302, { Location: `/?id=${title}` });
 				response.end();
 			});
 		});
 	} else if (pathname === '/update') {
-		fs.readdir('./data', function(error, filelist) {
+		fs.readdir('./data', function (error, filelist) {
 			fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
 				var title = queryData.id;
 				var list = templateList(filelist);
-				var template = templateHTML(title, list,
+				var template = templateHTML(
+					title,
+					list,
 					`<form action="/update_process" method="post">
 					<input type="hidden" name="id" value="${title}">
 					<p><input type="text" name="title" placeholder="Title" value="${title}"></p>
@@ -129,9 +137,9 @@ var app = http.createServer(function(request, response){
 			var id = post.id;
 			var title = post.title;
 			var description = post.description;
-			fs.rename(`data/${id}`, `data/${title}`, function(error){
+			fs.rename(`data/${id}`, `data/${title}`, function (error) {
 				fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-					response.writeHead(302, {Location: `/?id=${title}`});
+					response.writeHead(302, { Location: `/?id=${title}` });
 					response.end();
 				});
 			});
@@ -140,6 +148,5 @@ var app = http.createServer(function(request, response){
 		response.writeHead(404);
 		response.end('Not found');
 	}
-
 });
 app.listen(3000);
